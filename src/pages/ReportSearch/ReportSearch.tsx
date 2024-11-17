9251288c556aa29af821555cde071894fedc21f7
Big Pensil <johndoe@example.com>
2024-11-17 18:36:20 +0300

    ёбыные таблицы

--- src/pages/ReportSearch/ReportSearch.tsx
+++ src/pages/ReportSearch/ReportSearch.tsx
@@ -1,23 +1,43 @@
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { reportSearchResponse } from '@shared/mock/Marko';
 
 export const ReportSearch = () => {
+  const linkBodyTemplate = report => {
+    return (
+      <a
+        href={`${report.link}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Ссылка
+      </a>
+    );
+  };
+
   return (
     <DataTable
-      value={[]}
+      value={reportSearchResponse}
       tableStyle={{ minWidth: '50rem' }}
     >
       <Column field="language" header="Язык"></Column>
+      <Column field="numberPhrase" header="№"></Column>
       <Column field="phrase" header="Фраза"></Column>
       <Column
         field="search"
         header="Поисковая система"
       ></Column>
       <Column
-        field="document"
-        header="Документы 30.09.2024"
+        field="countDocuments"
+        header="Документы 28.09.2024"
       ></Column>
-      <Column field="link" header="Ссылки"></Column>
+      <Column
+        header="Ссылки"
+        field="link"
+        body={linkBodyTemplate}
+      >
+        <></>
+      </Column>
     </DataTable>
   );
 };
