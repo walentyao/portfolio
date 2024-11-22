dfdea96e190fe8f639557d1fb9c813491e338e36
Big Pensil <johndoe@example.com>
2024-11-22 20:18:58 +0300

    Раздел Библиотека

--- src/pages/Library/Library.tsx
+++ src/pages/Library/Library.tsx
@@ -1,9 +1,22 @@
+import { LibraryResponse } from '@shared/mock/Vala';
 import cls from './Library.module.scss';
+import { Card } from 'primereact/card';
 
 export const Library = () => {
+  const data = LibraryResponse;
   return (
-    <section aria-label="Library" className={cls.biography}>
-      Тут будут 3 ссылки
+    <section aria-label="Library">
+      <h3 className={cls.title}>
+        Библиотека материалов по теме выпускной работы
+      </h3>
+      {data.map(link => (
+        <Card className={cls.card}>
+          <p className={cls.cardTitle}>{link.title}</p>
+          <p className={cls.authors}>{link.authors}</p>
+          <p>{link.description}</p>
+          <p className={cls.authors}>{link.source}</p>
+        </Card>
+      ))}
     </section>
   );
 };
