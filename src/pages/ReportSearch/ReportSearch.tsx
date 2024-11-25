9dca6e0ca982729dd923670bf4194dab56c08b45
walentyao <prikolnyyyvalk@yandex.ru>
2024-11-25 19:36:15 +0300

    feat: добавлен английский

--- src/pages/ReportSearch/ReportSearch.tsx
+++ src/pages/ReportSearch/ReportSearch.tsx
@@ -10,17 +10,34 @@ export const ReportSearch = () => {
   const { lg } = useParams();
   const username = useContext(ContextUsername);
   const { reportSearchResponse } = user[username];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const linkBodyTemplate = (report: any) => {
+
+  const linkBodyTemplate = (
+    report: (typeof reportSearchResponse)[0],
+  ) => {
     return (
       <a
         href={`${report.link}`}
         target="_blank"
         rel="noopener noreferrer"
       >
-        Ссылка
+        {lg === 'ru' ? 'Ссылка' : 'Link'}
       </a>
     );
+  };
+
+  const renameCountDocuments = (
+    item: (typeof reportSearchResponse)[0],
+  ) => {
+    if (
+      typeof item.countDocuments === 'string' &&
+      item.countDocuments &&
+      lg === 'eng'
+    ) {
+      return item.countDocuments
+        .replace('тыс', 'thousands')
+        .replace('млн', 'millions');
+    }
+    return item.countDocuments;
   };
 
   return (
@@ -56,6 +73,7 @@ export const ReportSearch = () => {
               ? 'Документы 28.09.2024'
               : 'Documents 28.09.2024'
           }
+          body={renameCountDocuments}
         ></Column>
         <Column
           header={lg === 'ru' ? 'Ссылки' : 'Links'}
@@ -96,6 +114,7 @@ export const ReportSearch = () => {
               ? 'Документы 28.09.2024'
               : 'Documents 28.09.2024'
           }
+          body={renameCountDocuments}
         ></Column>
         <Column
           header={lg === 'ru' ? 'Ссылки' : 'Links'}
