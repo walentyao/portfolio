d69fb8b5bd12b92e205cd4bf189e6b5cd5ba635e
walentyao <prikolnyyyvalk@yandex.ru>
2025-01-17 02:42:06 +0300

    asd

--- src/app/providers/router/index.tsx
+++ src/app/providers/router/index.tsx
@@ -11,69 +11,72 @@ import { Uemk } from '@pages/Uemk/Uemk';
 import { AppRoutes } from '@shared/types';
 import { createHashRouter, Navigate } from 'react-router-dom';
 
-export const router = createHashRouter([
-  {
-    path: '/',
-    children: [
-      {
-        index: true,
-        element: <Navigate to={'ru/patokov'} />,
-      },
-      {
-        path: AppRoutes.MAIN,
-        children: [
-          {
-            index: true,
-            element: <Navigate to={'patokov'} />,
-          },
-          {
-            path: ':username',
-            element: <AppLayout />,
-            children: [
-              {
-                index: true,
-                element: <Main />,
-              },
-              {
-                path: AppRoutes.RESUME,
-                element: <Main />,
-              },
-              {
-                path: AppRoutes.BIOGRAFY,
-                element: <Biography />,
-              },
-              {
-                path: AppRoutes.REFERAT,
-                element: <Referat />,
-              },
-              {
-                path: AppRoutes.LIBRARY,
-                element: <Library />,
-              },
-              {
-                path: AppRoutes.LINKS,
-                element: <Links />,
-              },
-              {
-                path: AppRoutes.REPORT_SEARCH,
-                element: <ReportSearch />,
-              },
-              {
-                path: AppRoutes.IND_SECTION,
-                element: <Ind />,
-              },
-              {
-                path: AppRoutes.UEMK,
-                element: <Uemk />,
-              },
-              {
-                path: AppRoutes.TEST,
-                element: <Test />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+export const router = createHashRouter(
+  [
+    {
+      path: '/',
+      children: [
+        {
+          index: true,
+          element: <Navigate to={'ru/patokov'} />,
+        },
+        {
+          path: AppRoutes.MAIN,
+          children: [
+            {
+              index: true,
+              element: <Navigate to={'patokov'} />,
+            },
+            {
+              path: ':username',
+              element: <AppLayout />,
+              children: [
+                {
+                  index: true,
+                  element: <Main />,
+                },
+                {
+                  path: AppRoutes.RESUME,
+                  element: <Main />,
+                },
+                {
+                  path: AppRoutes.BIOGRAFY,
+                  element: <Biography />,
+                },
+                {
+                  path: AppRoutes.REFERAT,
+                  element: <Referat />,
+                },
+                {
+                  path: AppRoutes.LIBRARY,
+                  element: <Library />,
+                },
+                {
+                  path: AppRoutes.LINKS,
+                  element: <Links />,
+                },
+                {
+                  path: AppRoutes.REPORT_SEARCH,
+                  element: <ReportSearch />,
+                },
+                {
+                  path: AppRoutes.IND_SECTION,
+                  element: <Ind />,
+                },
+                {
+                  path: AppRoutes.UEMK,
+                  element: <Uemk />,
+                },
+                {
+                  path: AppRoutes.TEST,
+                  element: <Test />,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: '/portfolio' },
+);
